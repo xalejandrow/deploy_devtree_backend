6f93ac89056355286da810a6d71995178ffd35ce
Alew <xalejandrow@gmail.com>
2025-02-19 16:15:32 -0300

    Inferencia y Primitive Types en TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import colors from 'colors';
 import server from './server';
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || "4000";
 
 server.listen(port, () => {
   console.log(colors.blue.bold(`Servidor Funcionando en el puerto: ${port}`))
