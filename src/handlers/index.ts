795e08305a814b345e4aa524e4524b04b36d05c7
Alew <xalejandrow@gmail.com>
2025-02-19 17:18:26 -0300

    Añadiendo Types al Routing de Express

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,7 @@
+import { Request, Response } from "express";
 import User from "../models/User";
 
-export const createAccount = async (req, res) => {
+export const createAccount = async (req: Request, res: Response) => {
     // console.log(req.body);
     const user = new User(req.body);
     await user.save()
