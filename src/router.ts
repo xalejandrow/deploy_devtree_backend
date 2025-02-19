02bee2b50341339e7cf45f61ab59bfaf7386f8fc
Alew <xalejandrow@gmail.com>
2025-02-19 16:59:10 -0300

    Registro nuestro primer usuario en la base de datos

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import User from "./models/User";
 
 const router = Router();
 
 /** Autenticación y registro */
-router.post('/auth/register', (req, res) => {
-   console.log(req.body);
+router.post('/auth/register', async (req, res) => {
+   // console.log(req.body);
+   const user = new User(req.body);
+   await user.save();
+   
+   // Otra forma de hacerlo
+   // await User.create(req.body)
 });
 
 export default router;
