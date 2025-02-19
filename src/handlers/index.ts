0e8fa87a893838d88b1c08525878e7f0ff5af01f
Alew <xalejandrow@gmail.com>
2025-02-19 17:43:43 -0300

    Evitar Usuarios Duplicados

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,21 @@
-import { Request, Response } from "express";
+import  { Request, Response } from "express";
 import User from "../models/User";
 
 export const createAccount = async (req: Request, res: Response) => {
     // console.log(req.body);
-    const user = new User(req.body);
+
+    const { email } = req.body;
+
+    const userExists = await User.findOne({ email });
+    
+    if(userExists) {
+        const error = new Error('El usuario ya está registrado')
+        res.status(409).json({ error: error.message });
+        return;
+    }
+    
+    const user = new User(req.body)
     await user.save()
-    res.send('Registro Creado Correctamente')
+    res.status(200).send('Registro Creado Correctamente')
+
  }
