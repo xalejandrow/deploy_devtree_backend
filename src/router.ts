7387eb95ba554c509499739bb324fc12062e226e
Alew <xalejandrow@gmail.com>
2025-02-19 22:33:19 -0300

    Añadiendo el resto de validación al Registro de Usuarios

--- src/router.ts
+++ src/router.ts
@@ -6,7 +6,20 @@ const router = Router();
 
 /** Autenticación y registro */
 router.post('/auth/register',
-    body('handle').notEmpty(),
+    body('handle')
+        .notEmpty()
+        .withMessage('El handle no puede ir vacío'),
+    body('name')
+        .notEmpty()
+        .withMessage('El nombre no puede ir vacío'),
+    body('email')
+        .isEmail()
+        .withMessage('E-mail no válido'),
+    body('password')
+        .notEmpty()
+        .isLength({ min: 8 })
+        .withMessage('El password debe tener al menos 8 caracteres'),
+        // .withMessage('El password no puede ir vacío'),
     createAccount);
 
 export default router;
