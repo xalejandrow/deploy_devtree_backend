0bbd11653586042bae0a624e14294433a290d4e9
Alew <xalejandrow@gmail.com>
2025-02-21 18:26:43 -0300

    Habilitando CORS desde el backend

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback) {
-        console.log('origin', origin);
+        // console.log('origin', origin);
         
         if (origin === process.env.FRONTEND_URL) {
             callback(null, true);
