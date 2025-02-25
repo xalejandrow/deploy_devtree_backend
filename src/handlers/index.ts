3725392d82bc5413bc5a7ec1562c0d84e715284a
Alew <xalejandrow@gmail.com>
2025-02-25 10:22:56 -0300

    Creando un Endpoint en el backend para obtener el usuario Autenticado

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -71,3 +71,12 @@ export const login = async (req: Request, res: Response) => {
 
 
 }
+
+export const getUser = async (req: Request, res: Response) => {
+    const bearer = req.headers.authorization;
+    if(!bearer) {
+        const error = new Error('No Autorizado')
+        res.status(401).json({ error: error.message });
+        return 
+    }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import { createAccount, login } from "./handlers";
+import { createAccount, getUser, login } from "./handlers";
 import { handleInputErrors } from "./middleware/validation";
 
 const router = Router();
@@ -33,5 +33,7 @@ router.post('/auth/login',
     handleInputErrors,
     login
 );
+
+router.get('/user', getUser);
 
 export default router;
